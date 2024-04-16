46c1ef0be75b0ea94cc539c49abca5a54447d786
Ryan J Field <RyanJField@users.noreply.github.com>
2024-04-16 11:28:58 +0100

    Updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,13 @@
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+// const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+// const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+// function resizeIframe(iframe) {
+//     console.log('loaded iframe');
+//     //iframe.height = iframe.contentWindow.document.body.scrollHeight + "px";
+//     //window.requestAnimationFrame(() => resizeIframe(iframe));
+// }
+
+// const iframe = document.getElementById('app-iframe');
+// const handleLoad = () => console.log('loaded');
+
+// iframe.addEventListener('load', resizeIframe(iframe), true)
